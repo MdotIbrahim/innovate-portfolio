0e15df27d9723d7b88aeffb1736c90650cd1d0ec
Ibrahim <ibrahim.345xa@slmail.me>
2022-03-25 12:19:40 +0000

    added flask

---
+++ backend_project/javascript_companion_website/app.py
@@ -0,0 +1,41 @@
+from flask import Flask, Blueprint, render_template, url_for, redirect
+
+views = Blueprint(__name__, "views")
+app = Flask(__name__)
+app.register_blueprint(views, url_prefix = "/")
+
+@app.route("/") # access home page
+def home():   
+    return render_template("index.html")
+
+@app.route("/faq")
+def faq_page():
+    return render_template("faq.html") 
+
+@app.route("/about")
+def about_page():
+    return render_template("about.html")
+
+@app.route("/courses")
+def courses_page():
+    return render_template("courses.html")
+
+@app.route("/contact")
+def contact_page():
+    return render_template("contact.html") 
+    
+#######################################
+@app.route("/home")
+def home_redirect():
+    return redirect(url_for("home"))
+
+#######################################
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html") , 404
+
+#######################################
+
+if __name__ == "__main__":
+    app.run(debug = True, port = 8000)
